fec12550850d3b84c726aa5470fcfa53b14477fb
Pintu Prusty <pintuprusty16@gmail.com>
2023-09-02 05:32:27 +0530

    Add files via upload

---
+++ costum_array/index.js
@@ -0,0 +1,32 @@
+function array(){
+    // this.length=length
+    for(let i=0; i<arguments.length; i++){
+        Object.defineProperty(this, "length",{
+            value:0,
+            writable:true,
+            enumerable:false,
+            
+        })
+        this[i]=arguments[i]
+    }
+    this.length=arguments.length;
+    }
+
+    array.prototype.push= function(value){
+        this[this.length]=value;
+        this.length++
+    }
+    array.prototype.pop = function (){
+        if(this.length===0){
+            return undefined
+        }
+        let index=this.length-1;
+        delete this[index]
+        this.length--
+    }
+    let arr= new array();
+    arr.push('e')
+    arr.pop()
+    arr.pop()
+    console.log(Object.values(arr))
+    
